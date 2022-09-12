88d62d02ddee9d965a0144da145ea4210697222b
jaumee99 <jcasamitjana8@boscdelacoma.cat>
2022-09-12 16:30:58 +0200

    a

--- Uf1/HelloWorld/hola.php
+++ Uf1/HelloWorld/hola.php
@@ -1,8 +1,18 @@
-<html>
- <head>
-  <title>Prueba de PHP</title>
- </head>
- <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
- </body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<title>Pràctica hello world</title>
+
+</head>
+
+<body>
+<?php
+   echo( "<h1>Hello world</H1>" );
+?>
+<p>
+Benvinguts al curs de PHP.	
+</p>
+</body>
 </html>
