c616729e54857cfccfcba3579849013206a24321
jaumee99 <jcasamitjana8@boscdelacoma.cat>
2023-03-06 16:49:16 +0100

    google api i

---
+++ Uf2/BotDiscord/commands/pingPong.js
@@ -0,0 +1,8 @@
+function pingPong(message) {
+    if (message.content === 'ping') {
+      message.reply('pong');
+    }
+  }
+  
+  module.exports = pingPong;
+  
--- Uf2/BotDiscord/index.js
+++ Uf2/BotDiscord/index.js
@@ -1,41 +1,22 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv/config');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const pingPong = require('./commands/pingPong');
+const guardarMensaje = require('./commands/GuardaMissatge');
 
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	client.commands.set(command.data.name, command);
-}
-
-client.once(Events.ClientReady, () => {
-	console.log('Ready!');
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
 });
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+client.on('ready', () => {
+	console.log(`Ready!`);
+})
 
-	const command = client.commands.get(interaction.commandName);
+client.on('messageCreate', pingPong);
 
-	if (!command) return;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
-
-client.login(token);
+client.login(process.env.TOKEN);
