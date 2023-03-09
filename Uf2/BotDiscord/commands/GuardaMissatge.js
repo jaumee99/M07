e51ad2c23e06ffb9bc0cbabb7adf21b0b0af50d1
jaumee99 <jcasamitjana8@boscdelacoma.cat>
2023-03-09 18:41:15 +0100

    guarda els misatges en txt

--- Uf2/BotDiscord/commands/GuardaMissatge.js
+++ Uf2/BotDiscord/commands/GuardaMissatge.js
@@ -1,13 +1,39 @@
+const fs = require('fs');
 
 function guardarMensaje(message) {
 
     // Ignorar misatges del bot
     if (message.author.bot) return;
 
-    // data i hora actual
+    // Data i hora actual
     let date = new Date().toString();
 
-    console.log(`${message.author.username} ha enviat ${message} a les ${date}`);
+    // Data pel fitxer
+    let dia = new Date().getDate();
+    let mes = new Date().getMonth() + 1;
+    let any = new Date().getFullYear();
+
+
+    console.log(`\r"${message.author.username}" ha enviat "${message}" a les "${date}" en el xat "${message.channel.name}"`);
+
+    let logMessage = (`\r"${message.author.username}" ha enviat "${message}" a les "${date}" en el xat "${message.channel.name}"`);
+
+    //Crear carpeta on es guardara tot
+    if (!fs.existsSync('logs')) {
+        fs.mkdirSync('logs');
+    }
+
+    //cada vez que se envia un mensaje se crea un archivo con la fecha del dia
+    if (!fs.existsSync(`logs/${dia}_${mes}_${any}.txt`)) {
+        fs.writeFileSync(`logs/${dia}_${mes}_${any}.txt`, `Logs del dia ${dia} del mes ${mes} i any ${any}:`);
+    }
+
+    // Escriu en el fitxer de logs.txt
+    fs.appendFile(`logs/${dia}_${mes}_${any}.txt`, logMessage, (err) => {
+        if (err) throw err;
+    });
+
+    
 }
 
 module.exports = guardarMensaje;
